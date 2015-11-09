39125837f8d087225a9ef98bf641bd0dfb3d0e62
PhiLong <PhiLong@172.24.196.220>
2015-11-09 22:10:43 +0800

    Merge origin/master
    Conflicts:
    	MMS2Server-war/web/facilitySystem/FacilityOfficerPage/FacilityOfficerListFacility.jsp

--- MMS2Server-ejb/src/java/mms2/pos/session/SaleSessionWS.java
+++ MMS2Server-ejb/src/java/mms2/pos/session/SaleSessionWS.java
@@ -15,6 +15,9 @@ import javax.persistence.PersistenceContext;
 import mms2.pos.entity.ItemEntity;
 import mms2.pos.entity.SaleEntity;
 import java.lang.NullPointerException;
+import java.sql.Timestamp;
+import javax.persistence.Query;
+import mms2.customer.entity.CustomerEntity;
 
 /**
  *
@@ -23,26 +26,51 @@ import java.lang.NullPointerException;
 @WebService
 @Stateless
 public class SaleSessionWS {
+
     @PersistenceContext
     private EntityManager em;
+
     @WebMethod
-    public String createSale(@WebParam(name="sale")SaleEntity sale){
-        try{
-            ArrayList<Long> itemIdList =  sale.getItemIdList();
-            for(int i=0; i< itemIdList.size(); i++){
+    public String createSale(@WebParam(name = "sale") SaleEntity sale) {
+        try {
+            ArrayList<Long> itemIdList = sale.getItemIdList();
+            for (int i = 0; i < itemIdList.size(); i++) {
                 ItemEntity item = em.find(ItemEntity.class, itemIdList.get(i));
-                if(item.getQuantity()==0)
+                if (item.getQuantity() == 0) {
                     throw new NullPointerException();
+                }
             }
-            for(int i=0; i< itemIdList.size(); i++){
+            for (int i = 0; i < itemIdList.size(); i++) {
                 ItemEntity item = em.find(ItemEntity.class, itemIdList.get(i));
-                item.setQuantity(item.getQuantity()-1);
+                item.setQuantity(item.getQuantity() - 1);
                 em.merge(item);
+            }
+            //SET TIME STAMP
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            sale.setDate(currentTime);
+            //CALCULATE POINT
+            if (!sale.getCustomerEmail().isEmpty()) {
+                Query q = em.createQuery("SELECT cu FROM CustomerEntity cu WHERE cu.email = :inEmail");
+                q.setParameter("inEmail", sale.getCustomerEmail());
+                CustomerEntity customer = (CustomerEntity)q.getResultList().get(0);
+                String memberType  = customer.getMemberType();
+                int roundDown =  (int)  Math.floor(sale.getPrice());
+                int pointToAdd = 0;
+                if(memberType.equals("silver")){
+                    pointToAdd = roundDown;
+                }else if(memberType.equals("gold")){
+                    pointToAdd = roundDown*2;
+                }else{
+                    pointToAdd = roundDown*3;
+                }
+                customer.setPoints(pointToAdd+customer.getPoints());
+                sale.setPointsEarned(pointToAdd);
+                em.merge(customer);
             }
             em.persist(sale);
             em.flush();
             return sale.getId().toString();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             System.err.println("SaleSessionWS_createSale throw exception");
             ex.printStackTrace();
             return "Error";
